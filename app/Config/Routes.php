6dedf9b663ddb28dde8a1ee10434eaab97d3ad33
ELABASSI YASSINE <bassi.yassin54@gmail.com>
2024-12-31 01:22:40 +0100

    Login routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,3 +11,7 @@ $routes->get('/login', 'LoginController::index');// Affiche la page de connexion
 $routes->get('/signup', 'SignupController::index'); // Affiche la page d'inscription
 $routes->post('/signup', 'SignupController::register'); // Traite l'inscription
 
+// Routes pour la connexion
+$routes->get('/login', 'LoginController::index'); // Affiche la page de connexion
+$routes->post('/login', 'LoginController::authenticate'); // Traite la connexion
+$routes->get('/logout', 'LoginController::logout'); // Déconnecte l'utilisateur
